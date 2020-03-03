8e6ec6c24ca51073e199664f3cac7f3a5813e63b
Filippo <pinton.filippo@protonmail.com>
2020-03-03 17:56:42 +0100

    Fixed start commands

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import worker
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.call(['python', 'api.py'])
+    subprocess.call(['gunicorn', 'main:app'])
     subprocess.call(['python', 'worker'])
